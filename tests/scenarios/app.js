ac1340b10755d8d9b32f7223ef7958f22065768f
pansay <jan.krepelka@gmail.com>
2015-08-11 19:45:27 +0200

    e2e tests

--- tests/scenarios/app.js
+++ tests/scenarios/app.js
@@ -1,10 +1,35 @@
 describe('FlatAngle', function() {
 
-  it('should redirect to #posts', function() {
-    browser.get('/');
-    browser.getLocationAbsUrl().then(function(url) {
-        expect(url).toEqual('/posts');
-      });
-  });
+    it('should redirect to #posts', function() {
+        browser.get('/');
+        browser.getLocationAbsUrl().then(function(url) {
+            expect(url).toEqual('/posts');
+        });
+    });
+
+    it('should list all posts', function() {
+        var postsList = element.all(by.repeater('post in posts.list'));
+        expect(postsList.count()).toBeGreaterThan(0);
+    });
+
+    it('should load first post when clicked on its link', function() {
+
+        var postsList = element.all(by.repeater('post in posts.list'));
+        var link = postsList.get(0).$('a');
+        link.getAttribute('href').then(function(href) {
+            link.click();
+            browser.getCurrentUrl().then(function(url) {
+                expect(url).toEqual(href);
+            });
+        });
+
+    });
+
+    it('should redirect to #posts on 404', function() {
+        browser.get('#/posts/does-not-exist');
+        browser.getLocationAbsUrl().then(function(url) {
+            expect(url).toEqual('/posts');
+        });
+    });
 
 });
