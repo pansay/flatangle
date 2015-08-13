0abae193f39919e261960927d6947557a094ae37
pansay <jan.krepelka@gmail.com>
2015-08-13 07:54:11 +0200

    gen

--- generated/config.js
+++ generated/config.js
@@ -1,2 +1,2 @@
-var json = json || {};
-json["config"] = {"apiUrl": "https://api.github.com/repos/pansay/flatangle/contents/content/posts?ref=gh-pages"};
+var config = config || {};
+config["config"] = {		"apiUrl": "https://api.github.com/repos/pansay/flatangle/contents/content/posts?ref=gh-pages",	"lang": "en"};
--- generated/templates.js
+++ generated/templates.js
@@ -3,11 +3,11 @@ angular.module('templates-main', ['app/templates/views/details.html', 'app/templ
 angular.module("app/templates/views/details.html", []).run(["$templateCache", function($templateCache) {
   "use strict";
   $templateCache.put("app/templates/views/details.html",
-    "<a href=\"#/posts\">back to posts list</a><div class=\"post-content\" data-ng-show=\"post.details\" data-ng-bind-html=\"post.details\"></div>");
+    "<a href=\"#/posts\">{{texts.backToPostsList}}</a><div class=\"post-content\" data-ng-show=\"post.details\" data-ng-bind-html=\"post.details\"></div>");
 }]);
 
 angular.module("app/templates/views/list.html", []).run(["$templateCache", function($templateCache) {
   "use strict";
   $templateCache.put("app/templates/views/list.html",
-    "<h1>FlatAngle</h1><p>Posts list:</p><ul data-ng-show=\"posts.list\"><li data-ng-repeat=\"post in posts.list\"><a href=\"#\" data-ng-href=\"#/posts/{{post.alias}}\">{{post.date}} - {{post.alias}}</a></li></ul><p data-ng-hide=\"posts.list\">no posts yet</p>");
+    "<h1>{{texts.title}}</h1><p>{{texts.postsList}}</p><ul data-ng-show=\"posts.list\"><li data-ng-repeat=\"post in posts.list\"><a href=\"#\" data-ng-href=\"#/posts/{{post.alias}}\">{{post.date}} - {{post.alias}}</a></li></ul><p data-ng-hide=\"posts.list\">ø</p>");
 }]);
