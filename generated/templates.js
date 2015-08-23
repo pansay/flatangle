3e76b29f99fd2ee788ff69bc90af672208bda32e
pansay <jan.krepelka@gmail.com>
2015-08-23 10:35:27 +0200

    update gen

--- generated/templates.js
+++ generated/templates.js
@@ -3,11 +3,11 @@ angular.module('templates-main', ['app/templates/views/details.html', 'app/templ
 angular.module("app/templates/views/details.html", []).run(["$templateCache", function($templateCache) {
   "use strict";
   $templateCache.put("app/templates/views/details.html",
-    "<a href=\"#/posts\">{{texts.backToPostsList}}</a><div class=\"post-content\" data-ng-show=\"post.details\" data-ng-bind-html=\"post.details\"></div>");
+    "<a href=\"#/posts\">{{texts.backToPostsList}}</a><p>{{post.details.date|date:'longDate'}}</p><div class=\"post-content\" data-ng-bind-html=\"post.details.content\"></div>");
 }]);
 
 angular.module("app/templates/views/list.html", []).run(["$templateCache", function($templateCache) {
   "use strict";
   $templateCache.put("app/templates/views/list.html",
-    "<h1>{{texts.title}}</h1><p>{{texts.postsList}}</p><ul data-ng-show=\"posts.list\"><li data-ng-repeat=\"post in posts.list | orderBy: '-date'\"><a href=\"#\" data-ng-href=\"#/posts/{{post.alias}}\">{{post.date}} - {{post.alias}}</a></li></ul><p data-ng-hide=\"posts.list\">ø</p>");
+    "<h1>{{texts.title}}</h1><p>{{texts.postsList}}</p><ul data-ng-show=\"posts.list\"><li data-ng-repeat=\"post in posts.list | orderBy: '-date'\"><a href=\"#\" data-ng-href=\"#/posts/{{post.alias}}\">{{post.date}} - {{post.title||post.alias}}</a></li></ul><p data-ng-hide=\"posts.list\">ø</p>");
 }]);
